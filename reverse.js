424482784ebcf318dbcd22d10ea92e3fcd951b89
Ayatullah-khomeni <a.khomeni71@gmail.com>
2022-08-07 09:40:22 +0600

    added reverse way

---
+++ reverse.js
@@ -0,0 +1,26 @@
+// Target 1 to 10
+
+/* var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++
+} */
+
+
+// Target 10 to 1 : reverse way
+/* var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num--
+} */
+
+// Target 1 to 10
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// Target 10 to 1 : reverse way
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+} 
