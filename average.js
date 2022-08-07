fdbbb5d16256df1e641d6f5c49742b70b3d2f1d4
Ayatullah-khomeni <a.khomeni71@gmail.com>
2022-08-07 10:51:41 +0600

    added remainder

--- average.js
+++ average.js
@@ -1,2 +1,15 @@
-// 75.25, 65, 80, 35.45, 99.50
+// 75.25, 65, 80, 35.45, 99.50 
 // Mathematics, Biology, Chemistry, Physics, and Bangla
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+var totalMarks = totalMarks.toFixed(2);
+var totalMarks = parseFloat(totalMarks);
+console.log(totalMarks);
+var average = totalMarks / 5;
+var averageTwoDecimal = average.toFixed(2);
+var averageTwoDecimal = parseFloat(averageTwoDecimal);
+console.log(averageTwoDecimal);    
---
+++ remainder.js
@@ -0,0 +1,14 @@
+var number = 119;
+
+var remainder = number / 5;
+console.log(remainder);
+var remainder = number % 5;
+console.log(remainder);
+
+/* var remainder = remainder.toFixed(2);
+var remainder = parseFloat(remainder); */
+
+var i1 = 'I am going to be';
+var i2 = 'an awesome web developer';
+var totalI = i1 + ' ' + i2;
+console.log(totalI); 
